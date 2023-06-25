e6d257b1062617feb2b695ba74e7154dfcf345c2
stali1234 <stalin.sangeethraj@gmail.com>
2023-06-25 22:24:37 +0530

    added assets folder | parser middleware

---
+++ assets/js/home.js
@@ -0,0 +1,1 @@
+console.log("my scritp is loaded");
--- index.js
+++ index.js
@@ -1,11 +1,46 @@
 const express = require('express');
+const path = require('path'); // path
 const port = 8000;
 
 const app = express();
 
-app.get('/profile', function(req, res) {
-    console.log(req);
-    res.send('<h1>cool, it is running </h1>');
+app.set('view engine', 'ejs'); // this wil tell us the what template engine we are using .
+app.set('views', path.join(__dirname, 'views'))
+
+// middlerware
+
+app.use(express.urlencoded());
+app.use(express.static('assets'))
+
+var contactLIst = [{
+        name: "stalin",
+        phone: 2342423,
+    },
+    {
+        name: "sunny",
+        phone: 2342423,
+    },
+    {
+        name: "leone",
+        phone: 2342423,
+    },
+]
+
+app.get('/', function(req, res) {
+
+    return res.render('home', {
+        title: "Contact List",
+        contact_List: contactLIst
+    });
+})
+
+
+app.post('/create-contact', function(req, res) {
+    contactLIst.push({
+        name: req.body.name,
+        phone: req.body.phone
+    })
+    return res.redirect('/');
 })
 app.listen(port, function(err) {
 
